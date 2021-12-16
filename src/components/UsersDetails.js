af65e26f124d74cba85d838fe9d6bf351e2ca63e
Ravi Kumar <Ravi_kumar@blackbelthelp.com>
2021-12-16 13:36:47 +0530

    modified code

--- src/components/UsersDetails.js
+++ src/components/UsersDetails.js
@@ -4,15 +4,14 @@ import { Link } from 'react-router-dom';
 function UsersDetails(props) {
     const [data, setData] = useState(props.data);
     const [isfound, setIsFound] = useState(true);
+    const todoItems = data.map((item) => <li><Link to={"/user/" + item.id} key={item.id}>{item.name}</Link></li>)
     return (
         <div>
             <h1>React Dynamic Routing</h1>
             <ul>
-                {
-                    data.map((item) => <div><li><Link to={"/user/" + item.id}>{item.name}</Link></li></div>)
-                }
+                {todoItems}
             </ul>
-            <h3>This is current user id: {data.id}</h3>
+            <h3></h3>
         </div>
     )
 }
